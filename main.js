98dc39d34c0d26c7693c7fda91b7faba12dad84e
Farai Tanekha <30661410+codemusltd@users.noreply.github.com>
2021-09-13 19:16:10 +0100

    final commit

--- main.js
+++ main.js
@@ -1,5 +1,33 @@
 $(document).ready(
     () => {
-        
+        //update preview window with user input text from textarea
+        $('#text')
+        .on('keyup', ({currentTarget}) => {
+            $('.preview').html(
+                $(currentTarget).val()
+            )
+        })
+
+        //update preview window with user selected font style rules
+        $('#font')
+        .on('change', ({currentTarget}) => {
+            $('.preview').css(
+                'font-family', $(currentTarget).val()
+            )
+        })
+
+        $('#weight')
+        .on('change', ({currentTarget}) => {
+            $('.preview').css(
+                'font-weight', $(currentTarget).val()
+            )
+        })
+
+        $('#size')
+        .on('keyup', ({currentTarget}) => {
+            $('.preview').css(
+                'font-size', `${ $(currentTarget).val() }px`
+            )
+        })
     }
 )
